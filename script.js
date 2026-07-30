45f656fd1ec39f1a6fd8328f7aaee50da6744896
leiart2007 <elaine.viloria@student.laverdad.edu.ph>
2026-07-30 22:11:35 +0800

    add score var

---
+++ script.js
@@ -0,0 +1,6 @@
+let score = 0;
+
+const scoreDisplay = document.getElementById('score-display');
+const increaseBtn = document.getElementById('increase-btn');
+const decreaseBtn = document.getElementById('decrease-btn');
+const resetBtn = document.getElementById('reset-btn');
